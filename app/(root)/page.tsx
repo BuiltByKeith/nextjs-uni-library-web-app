7fe715d9aa97467178d558f842951f794a5d54b3
BuiltByKeith <builtbykeith.dev@gmail.com>
2025-04-29 20:18:51 +0800

    latest commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,19 @@
+import BookList from "@/components/BookList";
+import BookOverview from "@/components/BookOverview";
+import { sampleBooks } from "@/constants";
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <BookOverview {...sampleBooks[0]}></BookOverview>
+      <BookList
+        title={"Latest Books"}
+        books={sampleBooks}
+        containerClassName={"mt-28"}
+      ></BookList>
+    </>
+  );
+};
+
+export default Home;
---
+++ components/Header.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+
+const Header = () => {
+  const pathName = usePathname();
+  return (
+    <header className="my-10 flex justify-between gap-5">
+      <Link href={"/"} className="flex flex-row justify-center items-center gap-6">
+        <Image
+          src={"/icons/logo.svg"}
+          alt="logo"
+          width={40}
+          height={40}
+        ></Image>
+        <p className="font-bebas-neue font-semibold text-4xl text-primary">UniLib</p>
+      </Link>
+      <ul className="flex flex-row items-center gap-8">
+        <li>
+          <Link
+            href={"/library"}
+            className={cn(
+              "text-base cursor-pointer capitalize",
+              pathName === "/library" ? "text-light-200" : "text-light-100"
+            )}
+          >
+            Library
+          </Link>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
